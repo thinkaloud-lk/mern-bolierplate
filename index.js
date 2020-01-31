c737b880c03d638a4538fa39035a29704de8b518
Chinthaka Weerakkody <lk0858@linde.com>
2020-01-31 21:33:41 +0530

    mongoose-register

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config/keys');
+const { User } = require('./models/user');
 
 const app = express();
-const uris = "mongodb+srv://thinkaloud:M0!aMala@cluster0-fi8fh.mongodb.net/test?retryWrites=true&w=majority"
-mongoose.connect(uris, {
+
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
   .then(() => console.log('DB connected'))
   .catch(err => console.log('error connecting', err))
 
-app.listen(5000, () => console.log('server running in 5000'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello ado')
+app.get('/', (req, res) => res.send('server running'))
+
+app.post('/api/users/register', (req, res) => {
+  const newUser = new User(req.body);
+  newUser.save((err, doc) => {
+    if (err) return res.status(401).json({ success: false, error: err })
+    return res.status(200).json({ success: true })
+  })
 })
+
+app.listen(4000, () => console.log('server running in 4000'))
